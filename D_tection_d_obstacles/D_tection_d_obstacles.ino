27a2d5bfb34e5b61680a788b2985b60b4ae6de25
COOL TNT <cool.tnt@outlook.com>
2022-03-16 16:25:09 +0100

    Fonctionne

--- D_tection_d_obstacles/D_tection_d_obstacles.ino
+++ D_tection_d_obstacles/D_tection_d_obstacles.ino
@@ -4,12 +4,14 @@ const int echoPin = 4;
 // defines variables
 long duration;
 int distances[10];
-float variations[9];
+float dangerosite, verif1, verif2, timeM, timeA;
+String Danger;
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
+
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -18,39 +20,37 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distances[0] = duration * 0.034 / 2;
-  Serial.print("Distances: ");
   
 
   
   
-  for(int x = 0; x<9;x++){
-    variations[x] = distances[x] - distances[x+1];
-  }
-
-  if(distances[1] - distances[2] <= -100 && distances[0] < 100 distances[0] >= 70){
-    if(distances[0] - distances[2] <= -100){
-      //Danger Faible
+  verif1 = distances[1] - distances[2];
+  verif2 = distances[0] - distances[2];
+  timeA = millis();
+  if(verif1 - verif2 <= 5){
+    dangerosite = -verif1 * 10 / distances[1] * (timeA-timeM);
+    if(dangerosite > 0 && distances[0] < 100){
+      if(dangerosite > 3000){
+        Serial.println("Pop");
+      }
+      else if(35 < dangerosite && dangerosite <= 55){
+        Serial.println("Faible");
+      }
+      else if(55 < dangerosite && dangerosite <= 125){
+        Serial.println("Moyen");
+      }
+      else if(125 < dangerosite && dangerosite <= 3000){
+        Serial.println("Eleve");
+      }
+      Serial.println(dangerosite);
     }
   }
-  if(distances[1] - distances[2] <= -20 && distances[0] < 70 distances[0] >= 40){
-    if(distances[0] - distances[2] <= -20){
-      //Danger Moyen
-    }
-  }
-  if(distances[1] - distances[2] <= - && distances[0] < 40 distances[0] >= 10){
-    if(distances[0] - distances[2] <= -20){
-      //Danger Moyen
-    }
-  }
+  timeM = timeA;
   
-  for(int x = 0; x<9;x++){
-  Serial.println(variations[x]);
-  delay(50);
-  }
   distances[9] = 0;
   for(int x = 0; x<10;x++){
     distances[10-x] = distances[9-x];
   }
 
-  delay(1000);
+  delay(200);
 }
