3dcecd01ed8bc4a73b05b23d725248b21fbadaac
MoooLaaa <101727582+MoooLaaa@users.noreply.github.com>
2022-04-06 13:52:38 +0200

    Update ArduinoHM10Test.ino

--- ConnectionBluetooth/ArduinoHM10Test/ArduinoHM10Test.ino
+++ ConnectionBluetooth/ArduinoHM10Test/ArduinoHM10Test.ino
@@ -1,37 +1,22 @@
 int inByte = 0;         // incoming serial byte
 boolean ledStatus = false;
 
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3);
+
 void setup() {
   // start serial port at 9600 bps and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-    // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  mySerial.begin(9600);
 }
 
 void loop() {
-  // if we get a valid byte, read analog ins:
-  if (Serial.available() > 0) {
+  while (mySerial.available() > 0) {
     // get incoming byte:
-    inByte = Serial.read();
-    if (inByte == 0x01) {
-      if (ledStatus) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        Serial.print("Setting LED On");
-        ledStatus = false;
-      }
-      else {
-        digitalWrite(LED_BUILTIN, LOW);
-        Serial.print("Setting LED Off");
-        ledStatus = true;
-      }
-    }
-    else {
-      Serial.write(inByte);
-    }
+    inByte = mySerial.read();
+    Serial.println(inByte);
+    
   }
+  delay(200);
 }
