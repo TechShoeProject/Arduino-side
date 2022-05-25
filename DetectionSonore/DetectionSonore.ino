9f51c8aa541cf61b1e8a81d311eeba1bc8f80816
MoooLaaa <101727582+MoooLaaa@users.noreply.github.com>
2022-05-25 15:23:12 +0200

    Update DetectionSonore.ino

--- DetectionSonore/DetectionSonore.ino
+++ DetectionSonore/DetectionSonore.ino
@@ -1,14 +1,36 @@
-const int DO = 2, AO = A0;
-float son;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(AO, INPUT);
-  pinMode(DO, INPUT);
+// Déclaration et initialisation des broches d'entrées
+int Analog_Eingang = A0; // Entrée analogique
+int Digital_Eingang = 3; // Entrée digitale
+  
+void setup ()
+{
+  pinMode (Analog_Eingang, INPUT);
+  pinMode (Digital_Eingang, INPUT);
+       
+  Serial.begin (9600); // Sortie série à 9600 bauds
 }
-
-void loop() {
-  son = analogRead(AO);
-  Serial.println(son);
-  delay(20);
+  
+// Le programme lit les valeurs des broches d'entrée et les envoie à la sortie série
+void loop ()
+{
+  float Analog;
+  int Digital;
+    
+  //Les valeurs sont lues, sont converties en tension...
+  Analog = analogRead (Analog_Eingang) ; 
+  Digital = digitalRead (Digital_Eingang);
+    
+  //... et envoyées à la sortie série.
+  Serial.print ("Tension analogique:"); Serial.print (Analog, 4);  Serial.print ("V, ");
+  Serial.print ("Limite:");
+  
+  if(Digital==1)
+  {
+      Serial.println (" atteinte");
+  }
+  else
+  {
+      Serial.println (" pas encore atteinte");
+  }
+//delay(200);
 }
